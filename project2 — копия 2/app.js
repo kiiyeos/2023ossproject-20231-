7ef705d941da895b047addf8119bb03af2eab006
egamov giyos(기여스) <128561516+kiiyeos@users.noreply.github.com>
2023-06-09 13:19:01 +0900

    project
    new files
    
    Co-Authored-By: egamov <71968878+egamov@users.noreply.github.com>

---
+++ project2 — копия 2/app.js
@@ -0,0 +1,5 @@
+<i class="fa-brands fa-instagram fa-spin fa-spin-reverse" style="color: #0c402f;"></i>
+
+<i class="fa-brands fa-youtube fa-spin" style="color: #0c402f;"></i>
+
+<i class="fa-light fa-map-location-dot fa-spin" style="color: #12402e;"></i>
